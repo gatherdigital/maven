ebc7a2116792146d7d83a527684869faca86fd78
Matt Kennedy <mkennedy@fourclouds.com>
2015-02-04 13:48:54 -0500

    add build-maven script

---
+++ build-maven.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+BUILD_DIR=$PWD
+PROJECT_DIR=$1
+
+# Build artifacts
+cd $PROJECT_DIR
+mvn deploy -Dgithub.site.skip=true -Dmaven.site.skip=true -Dgpg.skip=true -DaltDeploymentRepository="snapshot-repo::default::file:$BUILD_DIR"
+
+# Create pretty directory listing
+cd $BUILD_DIR
+for DIR in $(find ./ \( -name build -o -name .git \) -prune -o -type d); do
+  (
+    echo "<html><body><h1>Directory listing</h1><hr/><pre>"
+    ls -1p "${DIR}" | grep -v "^\./$" | grep -v "build-" | grep -v "index.html" | awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }'
+    echo "</pre></body></html>"
+  ) > "${DIR}/index.html"
+done
+
+# Stage all files in git and create a commit
+git add --all .
+git commit -m "Built $(basename $PROJECT_DIR) on $(date)"
